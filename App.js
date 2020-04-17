bb275296649d6373a91c64f28f843c34904bcd38
mulx10 <mehulkumarnirala@iitkgp.ac.in>
2020-04-17 17:09:44 +0530

    Persist Redux Store

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react'
 import { ConfigureStore } from './redux/configureStore';
 import Main from './components/MainComponent';
+import { Loading } from './components/LoadingComponent';
 
 export default class App extends React.Component {
   render() {
-    const store = ConfigureStore();
+    const { persistor, store } = ConfigureStore();
 
     return (
       <Provider store={store}>
-         <Main />
+        <PersistGate
+          loading={<Loading />}
+          persistor={persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,17 +1,25 @@
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import {createStore } from 'redux';
-import { combineReducers } from 'redux';
+import { createStore } from 'redux';
+// import { combineReducers } from 'redux';
 import { applyMiddleware} from 'redux';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
 import { dishes } from './dishes';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { leaders } from './leaders';
 import { favorites } from './favorites';
 
+const config = {
+    key: 'root',
+    storage,
+    debug: true
+}
+
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             dishes,
             comments,
             promotions,
@@ -21,5 +29,6 @@ export const ConfigureStore = () => {
         applyMiddleware(thunk, logger)
     );
 
-    return store;
+    const persistor = persistStore(store)
+    return { persistor, store };
 }
